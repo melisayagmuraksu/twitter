0fc29bf107d68ff365c4116fd68b498d2b4c9f7b
melisayagmuraksu <melisayagmuraksu@gmail.com>
2026-07-27 00:02:32 +0300

    feat: add AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { Redirect, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { useContext } from "react";
+import { AuthContext } from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <>
       <div className="bg-black min-h-screen text-white">
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
-        <Route path="/home">
-          <Home />
+        <Route path="/login">
+          <Login />
         </Route>
+        <PrivateRoute path="/home">
+          <Home />
+        </PrivateRoute>
       </div>
     </>
   );
