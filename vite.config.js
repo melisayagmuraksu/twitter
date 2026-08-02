efc7eb29adce0b9bf16acb1c2fe5069542b6817d
melisayagmuraksu <melisayagmuraksu@gmail.com>
2026-08-02 15:27:34 +0300

    Fix GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: command === "serve" ? "/" : "/twitter/",
   test: {
-    // 👋 add the line below to add jsdom to vite
     environment: "jsdom",
-    // hey! 👋 over here
     globals: true,
-    setupFiles: "./src/__tests__/setup.js", // assuming the test folder is in the root of our project
+    setupFiles: "./src/__tests__/setup.js",
   },
-});
+}));
